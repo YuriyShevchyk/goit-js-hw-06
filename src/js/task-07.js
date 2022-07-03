b0a80da9d799bb2ce188a1d5d646b2ee0b2c901d
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-07-03 13:46:23 +0300

    task_7&8_complete

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -5,6 +5,6 @@ input.addEventListener('input', onChangeSpanText);
 
 function onChangeSpanText(event) {
     // input.scroll = event.currentTarget.value;
-    spanText.styles.fontSize = event.currentTarget.value + 'px';
+    spanText.style.fontSize = `${input.value}px`;
 }
 
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,10 @@
+// Напиши скрипт, який змінює кольори фону елемента
+//   < body > через інлайн - стиль по кліку на button.change - color
+//    і виводить значення кольору в span.color.
+const colorName = document.querySelector('.color')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let color = getRandomHexColor;
