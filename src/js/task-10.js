2d83543768051ae88fe436c5ab4ff8c59ad0ceac
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-07-04 15:54:28 +0300

    task10_complete

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const colorBox = document.querySelector('#boxes');
+const amount = document.querySelector('input');
+
+
+createButton.addEventListener('click', onCreateBoxes);
+destroyButton.addEventListener('click', onDectroyButton);
+
+function onCreateBoxes() {
+
+  let numberArray = [];
+  for (let i = 1; i <= amount.value; i += 1) {
+    numberArray.push(i);
+  };
+  // console.log(numberArray);
+  const boxes = numberArray.map((number) => {
+    const createBox  = document.createElement('div');
+    const value = (20 + number * 10);
+    createBox.style.width = `${value}px`;
+    createBox.style.height = `${value}px`;
+    createBox.style.backgroundColor = getRandomHexColor();
+    return createBox;
+    }); 
+  colorBox.append(...boxes);
+};
+
+function onDectroyButton() {
+  colorBox.innerHTML = '';
+  amount.value = '';
+}
+
